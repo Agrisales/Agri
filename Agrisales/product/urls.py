139db567a49f1a4962fb18d275c93bc82c9e2dad
Dhanushkumar S G <dhanushkumarganapathy@gmail.com>
2021-07-06 21:06:25 +0530

    progressing towards ..

--- Agrisales/product/urls.py
+++ Agrisales/product/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('seeds',views.seeds,name= "seeds"),
-    path('seeds/<str:seedname>', views.seeds, name="seedview"),
+    path('/seeds',views.seeds,name= "seeds"),
+    path('/seeds/<str:seedname>', views.seeds, name="seedview"),
     path('',views.display,name="display")
 ]
--- Agrisales/product/views.py
+++ Agrisales/product/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render ,redirect
+from . models import Product
 # Create your views here.
-def seeds(request,username = None ,seedname=None):
-    if seedname ==None:
-        return render(request, 'product/seeds.html')
-    else:
-        return render(request, 'product/seeds.html',{
-            'seedname': seedname,
-        })
+
 
 def display(request,username = None):
     if username == None:
@@ -16,3 +10,36 @@ def display(request,username = None):
         return render(request,'product/views.html',{
             'username':username
         })
+
+
+def search(request,username = None):
+    if request.method == "POST":
+        categary = ["seeds","fertilizers","fertilisers","pestisides","manures"]
+        search_value = request.POST.search
+        if search_value.lower() in categary:
+            url = f"product/{search_value}"
+            return render(request,url,{
+                'username':username,
+            })
+
+        products = Product.objects.all().filter(name = search_value.lower())
+        return render(request,'product/search.html',{
+            'products':products
+        })
+
+
+
+def seeds(request,username = None ,seedname=None):
+    if seedname ==None:
+        seeds = Product.objects.all()
+        return render(request, 'product/seeds.html',{
+            'seeds': seeds
+            })
+    else:
+        seeds = Product.objects.all().filter(name = seedname)
+        return render(request, 'product/seeds.html',{
+            'seeds': seeds
+        })
+
+
+
