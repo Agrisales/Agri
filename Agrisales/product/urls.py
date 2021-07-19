8ba1e38275de0be5261796888ad300d669aeef65
Dhanushkumar S G <dhanushkumarganapathy@gmail.com>
2021-07-19 19:27:13 +0530

    made some changes and also added product view page

--- Agrisales/product/urls.py
+++ Agrisales/product/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('/seeds',views.seeds,name= "seeds"),
-    path('/seeds/<str:seedname>', views.seeds, name="seedview"),
     path('',views.display,name="display"),
     path('/search',views.search, name="search"),
     path('/fertilizers',views.fertilizers,name= "fertilizers"),
     path('/pestisides',views.pestisides,name= "pestisides"),
     path('/manures',views.manures,name= "manures"),
+    path('/view/<str:productname>',views.view, name="view")
 
  
 ]
--- Agrisales/product/views.py
+++ Agrisales/product/views.py
@@ -15,66 +15,89 @@ def search(request,username = None):
         search_value = request.POST['search']
         if search_value.lower() in categary:
             if username != None:
-                url = f"{username}product/{search_value}"
+                url = f"/{username}/product/{search_value}"
             else:
                 url = f"/product/{search_value}"
             return redirect(url)
+        
+        if username:
+            products = Product.objects.all().filter(id = search_value.lower())
+            return render(request,'product/search.html',{
+                'products':products,
+                'username':username
+            })
 
-        products = Product.objects.all().filter(id = search_value.lower())
-        return render(request,'product/search.html',{
-            'products':products
-        })
+        else:
+            products = Product.objects.all().filter(id = search_value.lower())
+            return render(request,'product/search.html',{
+                'products':products
+            })
 
 
 
-def seeds(request,username = None ,seedname=None):
-    if seedname ==None:
+def seeds(request,username = None ):
+    if username :
         seeds = Product.objects.all().filter(category = 'seeds')
         return render(request, 'product/seeds.html',{
-            'seeds': seeds
+            'seeds': seeds,
+            'username':username
             })
     else:
-        seeds = Product.objects.all().filter(name = seedname)
+        seeds = Product.objects.all().filter(category = 'seeds')
         return render(request, 'product/seeds.html',{
-            'seeds': seeds
-        })
+            'seeds': seeds,
+            })
 
 
-def fertilizers(request,username = None ,fertilizername=None):
-    if fertilizername ==None:
+def fertilizers(request,username = None ):
+    if username :
         fertilizers = Product.objects.all().filter(category = 'fertilizers')
         return render(request, 'product/fertilizers.html',{
-            'fertilizers': fertilizers
+            'fertilizers': fertilizers,
+            'username':username
             })
     else:
-        fertilizers = Product.objects.all().filter(name = fertilizername)
+        fertilizers = Product.objects.all().filter(category = 'fertilizers')
         return render(request, 'product/fertilizers.html',{
-            'fertilizers': fertilizers
-        })
+            'fertilizers': fertilizers ,
+            })
 
 
-def pestisides(request,username = None ,pestisidename=None):
-    if pestisidename ==None:
+def pestisides(request,username = None):
+    if username :
         pestisides = Product.objects.all().filter(category = 'pestisides')
         return render(request, 'product/pestisides.html',{
-            'pestisides': pestisides
+            'pestisides': pestisides,
+            'username':username
             })
     else:
-        pestisides = Product.objects.all().filter(name = pestisidename)
+        pestisides = Product.objects.all().filter(category = 'pestisides')
         return render(request, 'product/pestisides.html',{
-            'pestisides': pestisides
-        })
+            'pestisides': pestisides,
+            })
 
-def manures(request,username = None ,manurename=None):
-    if manurename ==None:
+def manures(request,username = None):
+    if username :
         manures = Product.objects.all().filter(category = 'manures')
         return render(request, 'product/manures.html',{
-            'manures': manures
+            'manures': manures,
+            'username':username
             })
     else:
-        manures = Product.objects.all().filter(name = manurename)
+        manures = Product.objects.all().filter(category = 'manures')
         return render(request, 'product/manures.html',{
-            'manures': manures
+            'manures': manures,
+            })
+
+def view(request,productname,username=None):
+    if username:
+        product = Product.objects.get(id = productname)
+        return render(request,'product/view_product.html',{
+        'product':product,
+        'username':username
         })
-
-
+    else:
+        product = Product.objects.get(id = productname)
+        return render(request,'product/view_product.html',{
+        'product':product
+    })
