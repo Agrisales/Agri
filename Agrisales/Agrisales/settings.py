2ff888c594f28a68133acd1fb0e8c74469a4c739
Agrisales <84378326+Agrisales@users.noreply.github.com>
2021-08-10 19:13:51 +0530

    made some corrections

--- Agrisales/Agrisales/settings.py
+++ Agrisales/Agrisales/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'farmersfirst',
-        'USER': 'admin',
+        'USER': 'root',
         'PASSWORD': 'toor',
         'HOST': 'localhost',
         'PORT': '3306',
